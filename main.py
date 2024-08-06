0197b4578de5a1cd5abe43ae17bae66d7d7e21b8
Nastya Dudolad <a.dudolad09@gmail.com>
2024-08-06 13:49:26 +0300

    new version

--- main.py
+++ main.py
@@ -28,34 +28,3 @@ while running:
     screen.fill(BLACK)
 
     pygame.display.update()
-
-while running:
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                rect_speed_x = -2
-            if event.key == pygame.K_RIGHT:
-                rect_speed_x = 2
-            if event.key == pygame.K_UP:
-                rect_speed_y = -2
-            if event.key == pygame.K_DOWN:
-                rect_speed_y = 2
-        if event.type == pygame.KEYUP:
-            if event.key in [pygame.K_RIGHT, pygame.K_LEFT]:
-                rect_speed_x = 0
-            if event.key in [pygame.K_UP, pygame.K_DOWN]:
-                rect_speed_y = 0
-
-    rect_coords[0] += rect_speed_x
-    rect_coords[1] += rect_speed_y
-
-    screen. fill(BLACK)
-    pygame.draw.rect(screen, RED, rect_coords + rect_size, 0)
-    pygame.draw.circle(screen, GREEN, (100, 100), 30, 0)
-    pygame.display.update()
-    time.sleep(0.04)
-

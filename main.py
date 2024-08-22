1c8e11480cb03112da15f2b87aef4e9438cff025
Nastya Dudolad <a.dudolad09@gmail.com>
2024-08-22 15:08:28 +0300

    movement along the y and graphic picture

--- main.py
+++ main.py
@@ -18,28 +18,41 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((25, 40))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(player_img, (25, 40))
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
+        self.speedy = 0
 
     def update(self):
         self.speedx = 0
+        self.speedy = 0
         keystate = pygame.key.get_pressed()
 
         if keystate[pygame.K_LEFT]:
             self.speedx = -8
         if keystate[pygame.K_RIGHT]:
             self.speedx = 8
+        if keystate[pygame.K_UP]:
+            self.speedy = -8
+        if keystate[pygame.K_DOWN]:
+            self.speedy = 8
+
         if self.rect.right >= WIDTH:
             self.rect.right = WIDTH-10
         if self.rect.left <= 0:
             self.rect.left = 10
+        if self.rect.top <= 0:
+            self.rect.top = 10
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT-10
+
+
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
-
+player_img = pygame.image.load('img/space_shooter_pic.png')
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)

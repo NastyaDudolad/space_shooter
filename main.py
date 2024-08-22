d996870c77d21129857bb35129814e81f4c79d40
Nastya Dudolad <a.dudolad09@gmail.com>
2024-08-22 14:12:43 +0300

    add player sprite

--- main.py
+++ main.py
@@ -15,6 +15,34 @@ pygame.display.set_caption('My game')
 clock = pygame.time.Clock()
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((25, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH-10
+        if self.rect.left <= 0:
+            self.rect.left = 10
+        self.rect.x += self.speedx
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
 running = True
 
@@ -26,5 +54,6 @@ while running:
             running = False
 
     screen.fill(BLACK)
-
+    all_sprites.update()
+    all_sprites.draw(screen)
     pygame.display.update()
